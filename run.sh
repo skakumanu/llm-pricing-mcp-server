38bd46fc84a346e53f06ef0c215849ca2c700dc5
skakumanu <skakumanu@gmail.com>
2026-02-16 19:59:26 -0800

    Fix Azure startup dependency install

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/site/wwwroot
+python -m pip install --upgrade pip --quiet
+python -m pip install -r requirements.txt --quiet
+python -m uvicorn src.main:app --host 0.0.0.0 --port 8000
